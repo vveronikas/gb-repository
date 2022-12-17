2e4c8f4dd1fd4874ffbad178ac557cb4ea9f5e25
Veronika <vrserebryakova@gmail.com>
2022-12-17 20:42:29 +0100

    Builder

--- patterns/SBomberProject/SBomberProject/House.cpp
+++ patterns/SBomberProject/SBomberProject/House.cpp
@@ -33,16 +33,56 @@ bool House::isInside(double x1, double x2) const
 void House::Draw() const
 {
 	MyTools::SetColor(CC_Yellow);
-	GotoXY(x, y - 5);
-	cout << "  ########  ";
-	GotoXY(x, y - 4);
-	cout << "##        ##";
-	GotoXY(x, y - 3);
-	cout << "############";
-	GotoXY(x, y - 2);
-	cout << "#          #";
-	GotoXY(x, y - 1);
-	cout << "#          #";
-	GotoXY(x, y);
-	cout << "############";
+
+	HouseDirector hd;
+	HouseBuilderA houseA;
+	HouseBuilderB houseB;
+	hd.DrawHouse(houseB);
 }
+
+void HouseBuilder::CreateHouse()
+{
+	h = new House;
+	h->SetWidth(13);
+	h->SetPos(80, GetMaxY() - 6);
+};
+
+void HouseBuilderA::CreateFirstFloorWithWindow()
+{
+	GotoXY(h->GetX(), h->GetY() - 3);
+	cout << "############";
+	GotoXY(h->GetX(), h->GetY() - 2);
+	cout << "#          #";
+	GotoXY(h->GetX(), h->GetY() - 1);
+	cout << "#          #";
+	GotoXY(h->GetX(), h->GetY());
+	cout << "############";
+};
+
+void HouseBuilderA::CreatePipe()
+{
+	GotoXY(h->GetX(), h->GetY() - 5);
+	cout << "  ########  ";
+	GotoXY(h->GetX(), h->GetY() - 4);
+	cout << "##        ##";
+};
+
+void HouseBuilderB::CreateFirstFloor()
+{
+	GotoXY(h->GetX(), h->GetY() - 3);
+	cout << "############";
+	GotoXY(h->GetX(), h->GetY() - 2);
+	cout << "############";
+	GotoXY(h->GetX(), h->GetY() - 1);
+	cout << "############";
+	GotoXY(h->GetX(), h->GetY());
+	cout << "############";
+};
+
+void HouseBuilderB::CreatePipe()
+{
+	GotoXY(h->GetX(), h->GetY() - 5);
+	cout << "  ########  ";
+	GotoXY(h->GetX(), h->GetY() - 4);
+	cout << "############";
+};
--- patterns/SBomberProject/SBomberProject/House.h
+++ patterns/SBomberProject/SBomberProject/House.h
@@ -3,6 +3,10 @@
 #include <stdint.h>
 
 #include "DestroyableGroundObject.h"
+#include "MyTools.h"
+
+using namespace MyTools;
+using namespace std;
 
 class House : public DestroyableGroundObject
 {
@@ -19,3 +23,43 @@ private:
 	const uint16_t score = 40;
 };
 
+class HouseBuilder
+{
+public:
+	virtual ~HouseBuilder() { delete h; };
+	virtual void CreateFirstFloor() {};
+	virtual void CreateFirstFloorWithWindow() {};
+	virtual void CreatePipe() {};
+	virtual void CreateHouse();
+
+	House* GetHouse() const { return h; };
+
+protected:
+	House* h = nullptr;
+};
+
+class HouseBuilderA : public HouseBuilder
+{
+public:
+	void CreateFirstFloorWithWindow() override;
+	void CreatePipe() override;
+};
+
+class HouseBuilderB : public HouseBuilder
+{
+public:
+	void CreateFirstFloor() override;
+	void CreatePipe() override;
+};
+
+class HouseDirector
+{
+public:
+	void DrawHouse(HouseBuilder& builder)
+	{
+		builder.CreateHouse();
+		builder.CreateFirstFloor();
+		builder.CreateFirstFloorWithWindow();
+		builder.CreatePipe();
+	}
+};
